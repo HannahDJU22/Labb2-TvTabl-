bc827804e4a6e6c51705f72c5345ff0ad7144fae
hannah <hannah.degerheim@iths.se>
2023-09-14 15:54:54 +0200

    fixed programdata

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector(".menu-icon i");
 const menuList = document.querySelector(".menu");
 const channelHeading = document.getElementById('js-title');
 const channelSchedule = document.getElementById('js-schedule');
+const programTable = document.querySelector('#js-schedule');
 
 function toggleMenu() {
     if (menuList.style.display === "none" || menuList.style.display === "") {
@@ -26,16 +27,17 @@ function toggleMenu() {
             item.style.display = "none";
         });
         channelHeading.innerHTML = "";
+        channelSchedule.innerHTML = "";
     }
 }
 
 function setChannel(channelName) {
+    // createTable();
 
     switch (channelName) {
         case "SVT 1": {
             channelHeading.innerHTML = "SVT 1";
             getChannelSchedule(channelName);
-            //channelSchedule.innerHTML="<ul>";
             break;
         }
         case "SVT 2": {
@@ -53,21 +55,45 @@ function setChannel(channelName) {
             getChannelSchedule(channelName);
             break;
         }
-        case "SVT 24": {channelHeading.innerHTML = "SVT 24";
-        getChannelSchedule(channelName);    
-        break;
+        case "SVT 24": {
+            channelHeading.innerHTML = "SVT 24";
+            getChannelSchedule(channelName);
+            break;
         }
     }
 
 }
 function getChannelSchedule(channelName) {
     fetch(`data/${channelName}.json`)
-        //fetch('data/SVT 1.json')
         .then((response) => response.json())
         .then((data) => renderData(data));
 }
 
+// function renderData(scheduleData) {
+//     console.log("running renderData");
+//     channelSchedule.innerHTML = scheduleData;
+//     console.log(channelSchedule.innerHTML);
+// }
+
+// function createTable(){
+//     console.log("running createTable");
+//     console.log("programtable:" + programTable);
+//     programTable.innerHTML="<ul class='list-group list-group-flush'><li class='list-group-item show-previous'>Item 1</li><li class='list-group-item'>Item 2</li><li class='list-group-item'>Item 3</li></ul>";   
+//     console.log(programTable.innerHTML)
+// }
+
 function renderData(scheduleData) {
-    channelSchedule.innerHTML = JSON.stringify(scheduleData);
+    console.log(scheduleData);//detta är min array
+
+    let myData = scheduleData.map((item) => {
+        return `<ul class='list-group list-group-flush'>
+    <li class='list-group-item'>${item.name}</li><li class='list-group-item'>${item.start}</li></ul>`;
+    })
+    .join("");
+
+         programTable.innerHTML = "<li class='list-group-item show-previous'>Visa tidigare program</li>" + myData;
+
 }
+
+
 
