3c28da1ef73d7790f51bc39d25ba35c105fe0bc0
hannah <hannah.degerheim@iths.se>
2023-09-18 08:54:53 +0200

    fixed loading img

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const menuIcon = document.querySelector(".menu-icon i");
 const menuList = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu li");
 const channelHeading = document.getElementById('js-title');
 const channelSchedule = document.getElementById('js-schedule');
 const programTable = document.querySelector('#js-schedule');
@@ -9,12 +10,10 @@ const loadingImage = document.querySelector(".hidden");
 function toggleMenu() {
     if (menuList.style.display === "none" || menuList.style.display === "") {
         menuList.style.display = "block";
-
         menuList.classList.add("menu--show");
 
-        const menuItems = document.querySelectorAll(".menu li");
-        menuItems.forEach(function (item) {
-            item.style.display = "block";
+        menuItems.forEach(function (listItem) {
+            listItem.style.display = "block";
         });
 
         menuIcon.classList.remove("fa-bars");
@@ -24,12 +23,12 @@ function toggleMenu() {
         menuList.style.display = "none";
         menuIcon.classList.remove("fa-times");
         menuIcon.classList.add("fa-bars");
-        const menuItems = document.querySelectorAll(".menu li");
-        menuItems.forEach(function (item) {
-            item.style.display = "none";
+        
+        menuItems.forEach(function (listItem) {
+            listItem.style.display = "none";
         });
         channelHeading.innerHTML = "";
-        channelSchedule.innerHTML = "";
+        clear();
     }
 }
 
@@ -38,31 +37,41 @@ function setChannel(channelName) {
         case "SVT 1": {
             channelHeading.innerHTML = "SVT 1";
             getChannelSchedule(channelName);
+            clear();
             break;
         }
         case "SVT 2": {
             channelHeading.innerHTML = "SVT 2";
             getChannelSchedule(channelName);
+            clear();
             break;
         }
         case "SVT Barn": {
             channelHeading.innerHTML = "SVT Barn";
             getChannelSchedule(channelName);
+            clear();
             break;
         }
         case "Kunskapskanalen": {
             channelHeading.innerHTML = "Kunskapskanalen";
             getChannelSchedule(channelName);
+            clear();
             break;
         }
         case "SVT 24": {
             channelHeading.innerHTML = "SVT 24";
             getChannelSchedule(channelName);
+            clear();
             break;
         }
     }
 
 }
+
+function clear(){
+    channelSchedule.innerHTML = "";
+}
+
 async function getChannelSchedule(channelName) {
 
     console.log(loadingImage);
@@ -79,35 +88,44 @@ async function getChannelSchedule(channelName) {
     console.log(loadingImage);
 }
 
-//test-metod för att plocka ut timme och minut
-// function formatDate(dateFromAPI){
-// const newTime= new Date(dateFromAPI);
-// const hour=newTime.getHours();
-// const minute=newTime.getMinutes();
-
-// return `${hour}-${minute}`;
-// }
-
-
-//jag vill ha en funktion som formaterar start-strängen till att datum-objekt så att jag sedan kan plocka ut timme och minut
-
-
 function renderData(scheduleData) {
     //console.log(scheduleData);//detta är min array
+    const myTimeNow= new Date();
+    
+    //sorterar min array i tidsordning
+    scheduleData.sort((itemA, itemB) => {
+        const timeA = new Date(itemA.start);
+        const timeB = new Date(itemB.start);
+        
+        return timeA - timeB;
+    });
 
-    let myData = scheduleData.map((item) => {
+    //jag vill filtrera min array nu för att matcha mot dagens tid
+    //måste jämföra objekt mot objekt...
 
-        const dateObj=new Date(item.start);
-        const hours=dateObj.getHours().toString().padStart(2, '0');
-        const minutes=dateObj.getMinutes().toString().padStart(2, '0');
-        const newTime=`${hours}:${minutes}`;
-        item.newTime=newTime;
+    const filteredData = scheduleData.filter(item =>{
+        const programTime = new Date(item.start);
+        console.log(typeof programTime);
+        console.log(typeof myTimeNow);
+        
+        return programTime<myTimeNow;
+    });
 
-    console.log(newTime);
-
+    let myData = filteredData.map((item) => {
+        //formaterar om tiden till timmar+minuter
+        const programTime = new Date(item.start);
+        const hours = programTime.getHours().toString().padStart(2, '0');
+        const minutes = programTime.getMinutes().toString().padStart(2, '0');
+        const newTime = `${hours}:${minutes}`;
+        item.newTime = newTime;
+        console.log(typeof newTime);//ligger nu som sträng
+        
+        
+        //mappar in datan i min tabell
         return `<li class='list-group-item'><strong>${item.newTime}</strong><div>${item.name}</div></li>`;
     })
         .join("");
-
+    
+        //skriver ut datan till html-sidan
     programTable.innerHTML = "<ul class ='list-group list-group-flush'><li class='list-group-item show-previous'>Visa tidigare program</li>" + myData + "</ul>";
 }
